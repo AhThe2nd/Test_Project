ad3a43dacffb56baac6b69814c00a3084fe44f30
Andrew Howell <andrew.t.howell@gmail.com>
2023-01-05 09:55:12 -0330

    Updated main to push a change and ensure this is set up correctly.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This main has been updated and committed/pushed to GitHub");
     }
 }
